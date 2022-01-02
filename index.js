a32c302934689fbc8e23d94a8758acb0f297524c
ksw0154 <rlatkddnjs44@naver.com>
2022-01-03 00:02:21 +0900

    회원가입 기능

--- index.js
+++ index.js
@@ -23,18 +23,46 @@ mongoose
   .then(() => console.log("MongoDB conntected..."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  res.send("Hi");
+});
 
+// 회원가입
 app.post("/register", (req, res) => {
   // 회원가입할 때 필요한 정보들을 client에서 가져오면 데이터베이스에 넣어준다.
   const user = new User(req.body);
-
+  console.log(user);
   user.save((err, userInfo) => {
     // 실패했을 때
     if (err) return res.json({ success: false, err });
     // 성공했을 때
     return res.status(200).json({
       success: true,
+    });
+  });
+});
+
+app.post("/login", (req, res) => {
+  // 요청된 email을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+
+    // 요청한 email이 데이터베이스에 있다면 비밀번호가 같은지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+      }
+
+      // 비밀번호까지 같다면 Token 생성
+      user.generateToken((err, user) => {});
     });
   });
 });
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -30,6 +33,40 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+// mongoose의 save함수가 돌기 전에(index.js에서 user.save 직전)
+userSchema.pre("save", function (next) {
+  let user = this;
+  // user에서 password가 변경될 때만 아래의 내용을 실행한다.
+  // 새로 생성하는 user는 isModified가 무조건 true
+  if (user.isModified("password")) {
+    // 비밀번호를 암호화 시킨다.
+    // salt를 이용해서 비밀번호를 암호화해야 한다.
+    // salt를 먼저 생성 (saltRounds: 만들고 싶은 salt 자리수)
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      // salt 생성이 됐으면
+      // user.password: 내가 입력한 비밀번호
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+
+        // hash: 암호화 된 비밀번호
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, callback) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return callback(err);
+    callback(null, isMatch);
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
