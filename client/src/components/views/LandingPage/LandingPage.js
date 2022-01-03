e9bb866ed75b9aeea5548f4bacbe2c3f8957364d
ksw0154 <rlatkddnjs44@naver.com>
2022-01-04 00:19:15 +0900

    로그아웃 기능 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,23 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 import { loginUser } from "../../../_action/user_action";
+import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
+  const navigate = useNavigate();
   useEffect(() => {
     axios.get("/api/hello").then((response) => console.log(response));
   }, []);
+
+  const onClickHandler = () => {
+    axios.get(`/api/users/logout`).then((response) => {
+      if (response.data.success) {
+        navigate("/login");
+      } else {
+        alert("로그아웃이 실패했습니다.");
+      }
+    });
+  };
   return (
     <div
       style={{
@@ -17,6 +29,8 @@ const LandingPage = () => {
       }}
     >
       <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 };
